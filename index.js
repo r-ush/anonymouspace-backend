e6040182b75b4e2ab7c9c442f989fe38f086d3f9
r-ush <aarush.bhatt@gmail.com>
2020-10-17 00:49:12 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+var cors = require("cors");
+const Router = require("./src/routers/sample.router");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// app.use(cors())
+app.use(express.json());
+
+app.use("/router", Router);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is up!",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not Found");
+});
+
+app.listen(port, () => console.log(`App listening on port ${port} !`));
